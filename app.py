a3ecefc86a74bb098b5071fbfb9ed1e3c8f68969
whatcoloris <whatcoloris@gmail.com>
2022-10-22 23:12:50 -0400

    coming out of the woods

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+#from flask import Flask
+#app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('basic.html')
+#@app.route("/")
+#def index():
+#    return render_template('basic.html')
+
+from website import create_app
+
+app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True)
+    app.run(debug=True)
