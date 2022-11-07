a2a02855e20ad17727b5426d3819e0a73e1abd12
Brianna Adams <67566208+Brianna-Adams@users.noreply.github.com>
2022-11-07 08:53:27 -0500

    Add files via upload
    BA GUI update for Milestone 3: Monday, November 7, 2022

--- website/views.py
+++ website/views.py
@@ -5,13 +5,16 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("models.html")
+  return render_template("homepage.html") 
 
-@views.route('/models')
-def models():
-    return render_template("models.html")
-
+@views.route('/login') 
+def github1():
+    return render_template("githubauth.html") 
 
 @views.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("base.html")
+
+@views.route('/models')
+def models():
+   return render_template("models.html")
