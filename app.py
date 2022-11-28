b369ccb802069099a64b09de24a71c4fa43fa397
whatcoloris <whatcoloris@gmail.com>
2022-11-28 12:10:10 -0500

    beginning to work in gui

--- app.py
+++ app.py
@@ -1,13 +1,29 @@
-#from flask import Flask
-#app = Flask(__name__)
-
-#@app.route("/")
-#def index():
-#    return render_template('basic.html')
-
 from website import create_app
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from flask import Blueprint, render_template, Flask, redirect, url_for
+from flask_dance.contrib.github import make_github_blueprint, github
+
+dotenv_path = join(dirname(__file__), '../.env')
+load_dotenv(dotenv_path)
 
 app = create_app()
+
+app.secret_key = os.urandom(24)
+app.config["GITHUB_OAUTH_CLIENT_ID"] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
+app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get("GITHUB_OAUTH_CLIENT_SECRET")
+github_bp = make_github_blueprint()
+app.register_blueprint(github_bp, url_prefix="/login")
+
+@app.route("/login")
+def login():
+    if not github.authorized:
+        return redirect(url_for("github.login"))
+    resp = github.get("/user")
+    assert resp.ok
+    #return "You are @{login} on GitHub".format(login=resp.json()["login"])
+    return render_template("models.html",gh_json=resp.json())
 
 if __name__ == "__main__":
     app.run(debug=True)
--- website/views.py
+++ website/views.py
@@ -1,24 +1,10 @@
-import json
-import os
-from flask import Blueprint, render_template, Flask, redirect, url_for
-from flask_dance.contrib.github import make_github_blueprint, github
-
-app = Flask(__name__)
-app.secret_key = os.urandom(24)
-app.config["GITHUB_OAUTH_CLIENT_ID"] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
-app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get("GITHUB_OAUTH_CLIENT_SECRET")
-github_bp = make_github_blueprint()
-app.register_blueprint(github_bp, url_prefix="/login")
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
-@app.route("/")
+@views.route("/")
 def index():
-    if not github.authorized:
-        return redirect(url_for("github.login"))
-    resp = github.get("/user")
-    assert resp.ok
-    return "You are @{login} on GitHub".format(login=resp.json()["login"])
+    return 
 
 @views.route('/models')
 def models():
