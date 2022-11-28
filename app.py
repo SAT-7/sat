86979fd24ca84d964f4c14e79d0afc2d68774a2c
whatcoloris <whatcoloris@gmail.com>
2022-11-28 14:18:29 -0500

    got it so that users org membership loads, then user can choose which org to load, then the models page has that value, still need to attach to iframe

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from flask import Blueprint, render_template, Flask, redirect, url_for
 from flask_dance.contrib.github import make_github_blueprint, github
 
-dotenv_path = join(dirname(__file__), '../.env')
+dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 app = create_app()
@@ -13,17 +13,19 @@ app = create_app()
 app.secret_key = os.urandom(24)
 app.config["GITHUB_OAUTH_CLIENT_ID"] = os.environ.get("GITHUB_OAUTH_CLIENT_ID")
 app.config["GITHUB_OAUTH_CLIENT_SECRET"] = os.environ.get("GITHUB_OAUTH_CLIENT_SECRET")
-github_bp = make_github_blueprint()
+github_bp = make_github_blueprint(scope='read:org')
 app.register_blueprint(github_bp, url_prefix="/login")
 
-@app.route("/login")
-def login():
+@app.route("/connect")
+def connect():
     if not github.authorized:
         return redirect(url_for("github.login"))
-    resp = github.get("/user")
+    #resp = github.get("/user")
+    #resp = github.get("/user/repos")
+    resp = github.get("/user/memberships/orgs")
     assert resp.ok
     #return "You are @{login} on GitHub".format(login=resp.json()["login"])
-    return render_template("models.html",gh_json=resp.json())
-
+    return render_template("githubauth.html",gh_json=resp.json())
+    
 if __name__ == "__main__":
     app.run(debug=True)
--- website/views.py
+++ website/views.py
@@ -2,15 +2,23 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
-@views.route("/")
-def index():
-    return 
+@views.route('/')
+def home():
+  return render_template("homepage.html") 
 
-@views.route('/models')
-def models():
-    return render_template("models.html")
+@views.route('/home')
+def homepage():
+  return render_template("homepage.html") 
 
+#@views.route('/connect') 
+#def github1():
+#    return render_template("githubauth.html") 
+# NOTE:::: moved to app.py with some extra flask-dance stuff
 
 @views.route('/about')
 def about():
-    return render_template("about.html")
+    return render_template("base.html")
+
+@views.route('/models')
+def models():
+   return render_template("models.html")
