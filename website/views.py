b2ba1f02ea37ef03c99ba162d4c8d9dd9c3df8e6
whatcoloris <whatcoloris@gmail.com>
2022-12-10 16:31:51 -0500

    cleaned up a bit and merged a bit more

--- website/views.py
+++ website/views.py
@@ -17,34 +17,3 @@ def github1():
 @views.route('/about')
 def about():
     return render_template("base.html")
-
-@views.route('/models')
-def models():
-    num_agents = 55
-    uncertainty = 0.55
-    reevaluate_rate = 0.55
-    unit = 0.55
-    horizon = 5.5
-    max_noise = 55.0
-    sites = ['anatomyofamodel.html','osdcCoOpStripped.html']
-    with open('website/static/models/'+sites[0],'r') as file:
-        lines = file.readlines()
-    #print(lines)
-    count = 0
-    for line in lines:
-        if line.__contains__("-num_agents-"):
-            lines[count] = str(num_agents)+'\n'
-        if line.__contains__("-uncertainty-"):
-            lines[count] = str(uncertainty)+'\n'
-        if line.__contains__("-reevaluate_rate-"):
-            lines[count] = str(reevaluate_rate)+'\n'
-        if line.__contains__("-unit-"):
-            lines[count] = str(unit)+'\n'
-        if line.__contains__("-horizon-"):
-            lines[count] = str(horizon)+'\n'
-        if line.__contains__("-max_noise-"):
-            lines[count] = str(max_noise)+'\n'
-        count += 1
-    with open('website/static/models/currentmodel.html', 'w') as file:
-        file.writelines(lines)
-    return render_template("models.html")
