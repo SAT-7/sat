1b244ebd328331c953d903c46255592a1a41fbd9
whatcoloris <whatcoloris@gmail.com>
2022-12-10 15:52:24 -0500

    making test variables for both models

--- website/views.py
+++ website/views.py
@@ -9,14 +9,30 @@ def home():
 
 @views.route('/models')
 def models():
-    testval = 55
-    with open('website/static/models/anatomyofamodel.html','r') as file:
+    num_agents = 55
+    uncertainty = 0.55
+    reevaluate_rate = 0.55
+    unit = 0.55
+    horizon = 5.5
+    max_noise = 55.0
+    sites = ['anatomyofamodel.html','osdcCoOpStripped.html']
+    with open('website/static/models/'+sites[1],'r') as file:
         lines = file.readlines()
     #print(lines)
     count = 0
     for line in lines:
         if line.__contains__("-num_agents-"):
-            lines[count] = str(testval)+'\n'
+            lines[count] = str(num_agents)+'\n'
+        if line.__contains__("-uncertainty-"):
+            lines[count] = str(uncertainty)+'\n'
+        if line.__contains__("-reevaluate_rate-"):
+            lines[count] = str(reevaluate_rate)+'\n'
+        if line.__contains__("-unit-"):
+            lines[count] = str(unit)+'\n'
+        if line.__contains__("-horizon-"):
+            lines[count] = str(horizon)+'\n'
+        if line.__contains__("-max_noise-"):
+            lines[count] = str(max_noise)+'\n'
         count += 1
     with open('website/static/models/currentmodel.html', 'w') as file:
         file.writelines(lines)
