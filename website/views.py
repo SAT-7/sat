860205441556ce0e9f967ce51e5bf4dcd632e931
whatcoloris <whatcoloris@gmail.com>
2022-12-10 16:52:54 -0500

    duplicate connect routes

--- website/views.py
+++ website/views.py
@@ -10,10 +10,6 @@ def home():
 def homepage():
   return render_template("homepage.html") 
 
-@views.route('/connect') 
-def github1():
-    return render_template("githubauth.html")
-
 @views.route('/about')
 def about():
     return render_template("base.html")
