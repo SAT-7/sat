6b5262482fc00f1c6bafaca313c7e6288d37feb2
whatcoloris <whatcoloris@gmail.com>
2022-12-11 14:45:16 -0500

    trying a members_url

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def models():
         count += 1
     with open('website/static/models/currentmodel.html', 'w') as file:
         file.writelines(lines)
-    return render_template("models.html",gh_json=members.json()['members'])
+    return render_template("models.html",gh_json=members.json()['members_url'])
     
 def unused_code():
     entry = 0
