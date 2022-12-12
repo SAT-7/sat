ae84ae82892f245ecbdce4b48cc4089bc599db9e
whatcoloris <whatcoloris@gmail.com>
2022-12-12 16:44:25 -0500

    delete random py files

--- sat.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return render_template('homepage.html') #'basic.html'
-
-if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True)
